574bd65ae1e4dc6851bf72e5e822d20df4c4ceb6
maelcastagnier-svg <mael.castagnier@gmail.com>
2026-08-23 10:34:31 +0200

    chore: suppression route de debug temporaire (Hunting rarite verifiee)

--- app/api/debug/trigger-hunting-rarity/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from 'next/server'
-import { computeAndPersistTrapHuntingRankings } from '@/lib/pluton-hunting'
-
-export const maxDuration = 120
-
-export async function GET() {
-  const results = await computeAndPersistTrapHuntingRankings()
-  return NextResponse.json({ count: results.length, results })
-}
