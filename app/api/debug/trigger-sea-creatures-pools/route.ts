caa0fb12671a48495f2d35dcf88773141c2d364f
maelcastagnier-svg <mael.castagnier@gmail.com>
2026-08-23 10:49:15 +0200

    chore: force-dynamic sur route de debug sea-creatures (suspicion cache Next.js)

--- app/api/debug/trigger-sea-creatures-pools/route.ts
+++ app/api/debug/trigger-sea-creatures-pools/route.ts
@@ -2,6 +2,7 @@ import { NextResponse } from 'next/server'
 import { computeAndPersistSeaCreatureRankings } from '@/lib/pluton-sea-creatures'
 
 export const maxDuration = 280
+export const dynamic = 'force-dynamic'
 
 export async function GET() {
   const results = await computeAndPersistSeaCreatureRankings()
